2eb1724a5947b33ab292e1d6b84203aaa1536413
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-18 22:04:05 +0330

    added the google signin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Dashboard from "./Dashboard";
 import "bootstrap/dist/css/bootstrap.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <Dashboard />
+    <GoogleOAuthProvider clientId="54780398562-eacfgab9k31uvv514fkev3b12bbrhep4.apps.googleusercontent.com">
+      <div className="App">
+        <div className="container">
+          <Dashboard />
+        </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,21 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { googleLogout } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./Dashboard.css";
-import CheckEmail from "./CheckEmail";
+// import ShowDashboard from "./ShowDashboard";
+// import CheckEmail from "./CheckEmail";
 export default function Dashboard() {
-  const [email, setEmail] = useState("");
+  const onBtnClick = (e) => {
+    e.preventDefault();
+    googleLogout();
+  };
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    setEmail(event.target[0].value);
-  }
+  // const [email, setEmail] = useState("");
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   setEmail(event.target[0].value);
+  // }
   // function updateEmail(event) {
   //   event.preventDefault();
   //   setEmail(event.target.value);
   // }
 
   return (
-    <div className="Dashboard">
-      <form onSubmit={handleSubmit}>
+    <GoogleOAuthProvider clientId="465562680061-sk6v9r0i8cr3ocsk3i0063m3ajvkf8lp.apps.googleusercontent.com">
+      <div className="Dashboard">
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+        <button onClick={onBtnClick}>Logout</button>
+
+        {/* <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
             <input
@@ -34,7 +54,8 @@ export default function Dashboard() {
           </div>
         </div>
       </form>
-      <CheckEmail mail={email} />
-    </div>
+      <CheckEmail mail={email} /> */}
+      </div>
+    </GoogleOAuthProvider>
   );
 }
