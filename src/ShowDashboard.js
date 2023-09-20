da791d6e97be8c08843aead024402f08c28f0a67
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 19:53:30 +0330

    added 3d

--- src/ShowDashboard.js
+++ src/ShowDashboard.js
@@ -2,8 +2,14 @@ import React, { useState } from "react";
 import "./ShowDashboard.css";
 import Showinfo from "./Showinfo";
 import model from "./pictures/model.png";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 
 function ShowDashboard(props) {
+  function Model(props) {
+    const { scene } = useGLTF("./public/raspberry/source/rpi_L.obj");
+    return <primitive object={scene} {...props} />;
+  }
+
   // function CallShowinfo(event) {
   //   event.preventDefault();
   //   return <Showinfo />;
@@ -15,13 +21,31 @@ function ShowDashboard(props) {
   };
   return (
     <div className="ShowDashboard">
-      <img
+      {/* <img
         src={model}
         alt="model-3d"
         onClick={onBtnClick}
         className="img-fluid"
       />
-      {showChildComp && <Showinfo />}
+      {showChildComp && <Showinfo />} */}
+
+      <Canvas
+        dpr={[1, 2]}
+        shadows
+        camera={{ fav: 45 }}
+        style={{ position: "absolute" }}
+      >
+        <PresentationControls
+          speed={1.5}
+          global
+          zoom={0.5}
+          polar={[-0.1, Math.PI / 4]}
+        >
+          <Stage environment={null}>
+            <Model scale={0.01} />
+          </Stage>
+        </PresentationControls>
+      </Canvas>
     </div>
   );
   // return (
