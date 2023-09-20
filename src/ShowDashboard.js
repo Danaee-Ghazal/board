187d6290d51805a819e6bcf19cfe1ef74bc47572
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 22:09:11 +0330

    Update ShowDashboard.js

--- src/ShowDashboard.js
+++ src/ShowDashboard.js
@@ -1,45 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShowDashboard.css";
-// import Showinfo from "./Showinfo";
-// import model from "./pictures/model.png";
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { useLoader } from "@react-three/fiber";
+import Showinfo from "./Showinfo";
+import model from "./pictures/model.png";
+// import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+// import { useLoader } from "@react-three/fiber";
 
 function ShowDashboard(props) {
-  function Scene() {
-    const obj = useLoader(OBJLoader, "./public/raspberry/source/rpi_L.obj");
-    return <primitive object={obj} />;
-  }
-  // function CallShowinfo(event) {
-  //   event.preventDefault();
-  //   return <Showinfo />;
+  // function Scene() {
+  //   const obj = useLoader(OBJLoader, "./public/raspberry/source/rpi_L.obj");
+  //   return <primitive object={obj} />;
   // }
-  // const [showChildComp, setShowChildComp] = useState(false);
-  // const onBtnClick = (e) => {
-  //   e.preventDefault();
-  //   setShowChildComp((prevState) => !prevState);
-  // };
-  Scene();
+
+  const [showChildComp, setShowChildComp] = useState(false);
+  const onBtnClick = (e) => {
+    e.preventDefault();
+    setShowChildComp((prevState) => !prevState);
+  };
+  // Scene();
   return (
     <div className="ShowDashboard">
-      {/* <img
+      <img
         src={model}
         alt="model-3d"
         onClick={onBtnClick}
         className="img-fluid"
       />
-      {showChildComp && <Showinfo />} */}
+      {showChildComp && <Showinfo />}
     </div>
   );
-  // return (
-  //   <div className="ShowDashboard">
-
-  //     {/* <img
-  //       src={model}
-  //       onClick={CallShowinfo}
-  //       style={{ "pointer-events": "all" }}
-  //     /> */}
-  //   </div>
-  // );
 }
 export default ShowDashboard;
