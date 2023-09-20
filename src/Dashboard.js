bb44a66f5c80a0170742336adf1b4899e26bd8dc
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 14:39:17 +0330

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import jwt_decode from "jwt-decode";
-import { useGoogleLogin } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 // import { googleLogout } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./Dashboard.css";
@@ -9,13 +9,9 @@ import "./Dashboard.css";
 export default function Dashboard() {
   const clientId =
     "465562680061-sk6v9r0i8cr3ocsk3i0063m3ajvkf8lp.apps.googleusercontent.com";
-  // const showUserInformation = (response) => {
-  //   console.log(jwt_decode(response.credential.email));
-  // };
-  const login = useGoogleLogin({
-    onSuccess: (response) => console.log(jwt_decode(response.credential)),
-    flow: "auth-code",
-  });
+  const showUserInformation = (response) => {
+    console.log(jwt_decode(response.credential.email));
+  };
   // const onBtnClick = (e) => {
   //   e.preventDefault();
   //   googleLogout();
@@ -32,7 +28,12 @@ export default function Dashboard() {
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <div className="Dashboard">
-        <button onClick={() => login()}>Sign in with Google 🚀 </button>;
+        <GoogleLogin
+          onSuccess={showUserInformation}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
         {/* <button onClick={onBtnClick}>Logout</button> */}
         {/* <form onSubmit={handleSubmit}>
         <div className="row">
