1fe5fe49c81d4a244d26d93358ab837d10faa3fd
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 15:06:10 +0330

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -11,7 +11,7 @@ export default function Dashboard() {
   const clientId =
     "465562680061-sk6v9r0i8cr3ocsk3i0063m3ajvkf8lp.apps.googleusercontent.com";
   const showUserInformation = (response) => {
-    console.log(jwt_decode(response.email));
+    console.log(jwt_decode(response["email"]));
     // <Component data={response.credential} />;
   };
   // const onBtnClick = (e) => {
