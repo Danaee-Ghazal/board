35f178c2de3eb621d79c46c5a1bc0f38f486eab6
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 22:20:30 +0330

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,7 +5,8 @@ import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./Dashboard.css";
 // import CheckEmail from "./CheckEmail";
-import ShowDashboard from "./ShowDashboard";
+// import ShowDashboard from "./ShowDashboard";
+import CheckEmail from "./CheckEmail";
 
 export default function Dashboard() {
   // const [email, setEmail] = useState("");
@@ -13,6 +14,7 @@ export default function Dashboard() {
     "465562680061-sk6v9r0i8cr3ocsk3i0063m3ajvkf8lp.apps.googleusercontent.com";
   const showUserInformation = (response) => {
     console.log(jwt_decode(response.credential));
+    console.log(typeof JSON.parse(jwt_decode(response.credential)).email);
     // setEmail(String(JSON.parse(jwt_decode(response.credential)).email));
     // <CheckEmail data={email} />;
   };
@@ -33,7 +35,7 @@ export default function Dashboard() {
           />
         </div>
       </GoogleOAuthProvider>
-      <ShowDashboard />
+      <CheckEmail />
     </div>
   );
 }
