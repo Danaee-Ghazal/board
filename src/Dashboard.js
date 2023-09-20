980deff58ba218445d04ec7932db4532664ceef8
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 22:06:13 +0330

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -21,14 +21,6 @@ export default function Dashboard() {
   //   googleLogout();
   // };
 
-  // function handleSubmit(event) {
-  //   event.preventDefault();
-  //   setEmail(event.target[0].value);
-  // }
-  // function updateEmail(event) {
-  //   event.preventDefault();
-  //   setEmail(event.target.value);
-  // }
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <div className="Dashboard">
@@ -39,29 +31,6 @@ export default function Dashboard() {
           }}
         />
         <ShowDashboard />
-
-        {/* // <button onClick={onBtnClick}>Logout</button>
-        //{" "} */}
-        {/* <form onSubmit={handleSubmit}>
-    //       <div className="row">
-    //         <div className="col-9">
-    //           <input
-    //             type="search"
-    //             placeholder="Type the email please..."
-    //             className="form-control"
-    //             autoFocus="on"
-    //           ></input>
-    //         </div>
-    //         <div className="col-3">
-    //           <input
-    //             type="submit"
-    //             value="Search"
-    //             className="btn btn-primary w-100"
-    //           ></input>
-    //         </div>
-    //       </div>
-    //     </form>
-    //     <CheckEmail mail={email} /> */}
       </div>
     </GoogleOAuthProvider>
   );
