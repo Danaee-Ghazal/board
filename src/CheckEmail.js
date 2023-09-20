b2358f7c63b877da336f355dbbdc7294f77107de
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 22:13:49 +0330

    Update CheckEmail.js

--- src/CheckEmail.js
+++ src/CheckEmail.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ShowDashboard from "./ShowDashboard";
-export default function CheckEmail(props) {
-  // let list = [];
+export default function CheckEmail() {
+  // let validatedUsers = [];
+  // if (validatedUsers.indexOf({props.data}) !== -1)
   if (true) {
     return (
       <div className="CheckEmail">
-        Account {props.data} is valid.
+        Account is valid.
         <ShowDashboard />
       </div>
     );
