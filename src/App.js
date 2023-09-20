69510dc318004c763b263badb2560c349caa4959
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 21:37:08 +0330

    changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 import Dashboard from "./Dashboard";
 import "bootstrap/dist/css/bootstrap.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+
 function App() {
   return (
-    <GoogleOAuthProvider clientId="54780398562-eacfgab9k31uvv514fkev3b12bbrhep4.apps.googleusercontent.com">
-      <div className="App">
-        <div className="container">
-          <Dashboard />
-        </div>
+    <div className="App">
+      <div className="container">
+        <Dashboard />
       </div>
-    </GoogleOAuthProvider>
+    </div>
   );
 }
 
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,14 +4,16 @@ import { GoogleLogin } from "@react-oauth/google";
 // import { googleLogout } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./Dashboard.css";
-// import ShowDashboard from "./ShowDashboard";
 import CheckEmail from "./CheckEmail";
+// import ShowDashboard from "./ShowDashboard";
+
 export default function Dashboard() {
   const [email, setEmail] = useState("");
   const clientId =
     "465562680061-sk6v9r0i8cr3ocsk3i0063m3ajvkf8lp.apps.googleusercontent.com";
   const showUserInformation = (response) => {
     setEmail(String(JSON.parse(jwt_decode(response.credential)).email));
+    <CheckEmail data={email} />;
   };
   // const onBtnClick = (e) => {
   //   e.preventDefault();
@@ -35,28 +37,29 @@ export default function Dashboard() {
             console.log("Login Failed");
           }}
         />
-        <CheckEmail data={email} />
-        {/* <button onClick={onBtnClick}>Logout</button> */}
+
+        {/* // <button onClick={onBtnClick}>Logout</button>
+        //{" "} */}
         {/* <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type the email please..."
-              className="form-control"
-              autoFocus="on"
-            ></input>
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            ></input>
-          </div>
-        </div>
-      </form>
-      <CheckEmail mail={email} /> */}
+    //       <div className="row">
+    //         <div className="col-9">
+    //           <input
+    //             type="search"
+    //             placeholder="Type the email please..."
+    //             className="form-control"
+    //             autoFocus="on"
+    //           ></input>
+    //         </div>
+    //         <div className="col-3">
+    //           <input
+    //             type="submit"
+    //             value="Search"
+    //             className="btn btn-primary w-100"
+    //           ></input>
+    //         </div>
+    //       </div>
+    //     </form>
+    //     <CheckEmail mail={email} /> */}
       </div>
     </GoogleOAuthProvider>
   );
