59eafaa5cf6053c0cb4a2e18aefb9a35a09beccd
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 21:58:20 +0330

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import jwt_decode from "jwt-decode";
+import React from "react";
+// import jwt_decode from "jwt-decode";
 import { GoogleLogin } from "@react-oauth/google";
 // import { googleLogout } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./Dashboard.css";
-import CheckEmail from "./CheckEmail";
-// import ShowDashboard from "./ShowDashboard";
+// import CheckEmail from "./CheckEmail";
+import ShowDashboard from "./ShowDashboard";
 
 export default function Dashboard() {
-  const [email, setEmail] = useState("");
+  // const [email, setEmail] = useState("");
   const clientId =
     "465562680061-sk6v9r0i8cr3ocsk3i0063m3ajvkf8lp.apps.googleusercontent.com";
   const showUserInformation = (response) => {
-    setEmail(String(JSON.parse(jwt_decode(response.credential)).email));
-    <CheckEmail data={email} />;
+    // setEmail(String(JSON.parse(jwt_decode(response.credential)).email));
+    // <CheckEmail data={email} />;
   };
   // const onBtnClick = (e) => {
   //   e.preventDefault();
@@ -37,6 +37,7 @@ export default function Dashboard() {
             console.log("Login Failed");
           }}
         />
+        <ShowDashboard />
 
         {/* // <button onClick={onBtnClick}>Logout</button>
         //{" "} */}
