0cf60adf9442c80dfb397700cc5a49bf21e66c5f
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-09-20 22:01:23 +0330

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import jwt_decode from "jwt-decode";
+import jwt_decode from "jwt-decode";
 import { GoogleLogin } from "@react-oauth/google";
 // import { googleLogout } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
@@ -12,6 +12,7 @@ export default function Dashboard() {
   const clientId =
     "465562680061-sk6v9r0i8cr3ocsk3i0063m3ajvkf8lp.apps.googleusercontent.com";
   const showUserInformation = (response) => {
+    console.log(jwt_decode(response.credential));
     // setEmail(String(JSON.parse(jwt_decode(response.credential)).email));
     // <CheckEmail data={email} />;
   };
